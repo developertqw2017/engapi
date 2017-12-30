5fe0d60196c4b719c47877d02a7bfb66c96f5ab4
developertqw2017 <2144799613@qq.com>
2017-12-30 21:06:01 +0800

    xiugai

--- englishstu/settings.py
+++ englishstu/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'user',
+    'login',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +77,12 @@ WSGI_APPLICATION = 'englishstu.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'englishstu',
+        'USER': 'root',
+        'PASSWORD': 'tqw503417',
+        'HOST': 'localhost',
+        'PORT':'3306',
     }
 }
 
